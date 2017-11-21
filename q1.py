7317539a80d9bbf8414ba931551495158c72f2ae
leehh <leehh@leehhs-MacBook-Air.local>
2017-11-21 18:03:39 -0500

    q1&q2

---
+++ q1.py
@@ -0,0 +1,60 @@
+'''
+Question 1 Skeleton Code
+
+
+'''
+
+import sklearn
+import numpy as np
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.naive_bayes import BernoulliNB
+
+def load_data():
+    # import and filter data
+    newsgroups_train = fetch_20newsgroups(subset='train',remove=('headers', 'footers', 'quotes'))
+    newsgroups_test = fetch_20newsgroups(subset='test',remove=('headers', 'footers', 'quotes'))
+
+    return newsgroups_train, newsgroups_test
+
+def bow_features(train_data, test_data):
+    # Bag-of-words representation
+    bow_vectorize = CountVectorizer()
+    bow_train = bow_vectorize.fit_transform(train_data.data) #bag-of-word features for training data
+    bow_test = bow_vectorize.transform(test_data.data)
+    feature_names = bow_vectorize.get_feature_names() #converts feature index to the word it represents.
+    shape = bow_train.shape
+    print('{} train data points.'.format(shape[0]))
+    print('{} feature dimension.'.format(shape[1]))
+    print('Most common word in training set is "{}"'.format(feature_names[bow_train.sum(axis=0).argmax()]))
+    return bow_train, bow_test, feature_names
+
+def tf_idf_features(train_data, test_data):
+    # Bag-of-words representation
+    tf_idf_vectorize = TfidfVectorizer()
+    tf_idf_train = tf_idf_vectorize.fit_transform(train_data.data) #bag-of-word features for training data
+    feature_names = tf_idf_vectorize.get_feature_names() #converts feature index to the word it represents.
+    tf_idf_test = tf_idf_vectorize.transform(test_data.data)
+    return tf_idf_train, tf_idf_test, feature_names
+
+def bnb_baseline(bow_train, train_labels, bow_test, test_labels):
+    # training the baseline model
+    binary_train = (bow_train>0).astype(int)
+    binary_test = (bow_test>0).astype(int)
+
+    model = BernoulliNB()
+    model.fit(binary_train, train_labels)
+
+    #evaluate the baseline model
+    train_pred = model.predict(binary_train)
+    print('BernoulliNB baseline train accuracy = {}'.format((train_pred == train_labels).mean()))
+    test_pred = model.predict(binary_test)
+    print('BernoulliNB baseline test accuracy = {}'.format((test_pred == test_labels).mean()))
+
+    return model
+
+if __name__ == '__main__':
+    train_data, test_data = load_data()
+    train_bow, test_bow, feature_names = bow_features(train_data, test_data)
+
+    bnb_model = bnb_baseline(train_bow, train_data.target, test_bow, test_data.target)
---
+++ q2.py
@@ -0,0 +1,150 @@
+import numpy as np 
+
+from sklearn.datasets import fetch_mldata
+import matplotlib.pyplot as plt
+
+np.random.seed(1847)
+
+class BatchSampler(object):
+    '''
+    A (very) simple wrapper to randomly sample batches without replacement.
+
+    You shouldn't need to touch this.
+    '''
+    
+    def __init__(self, data, targets, batch_size):
+        self.num_points = data.shape[0]
+        self.features = data.shape[1]
+        self.batch_size = batch_size
+
+        self.data = data
+        self.targets = targets
+
+        self.indices = np.arange(self.num_points)
+
+    def random_batch_indices(self, m=None):
+        '''
+        Get random batch indices without replacement from the dataset.
+
+        If m is given the batch will be of size m. Otherwise will default to the class initialized value.
+        '''
+        if m is None:
+            indices = np.random.choice(self.indices, self.batch_size, replace=False)
+        else:
+            indices = np.random.choice(self.indices, m, replace=False)
+        return indices 
+
+    def get_batch(self, m=None):
+        '''
+        Get a random batch without replacement from the dataset.
+
+        If m is given the batch will be of size m. Otherwise will default to the class initialized value.
+        '''
+        indices = self.random_batch_indices(m)
+        X_batch = np.take(self.data, indices, 0)
+        y_batch = self.targets[indices]
+        return X_batch, y_batch  
+
+class GDOptimizer(object):
+    '''
+    A gradient descent optimizer with momentum
+    '''
+
+    def __init__(self, lr, beta=0.0):
+        self.lr = lr
+        self.beta = beta
+
+    def update_params(self, params, grad):
+        # Update parameters using GD with momentum and return
+        # the updated parameters
+        return None
+
+class SVM(object):
+    '''
+    A Support Vector Machine
+    '''
+
+    def __init__(self, c, feature_count):
+        self.c = c
+        self.w = np.random.normal(0.0, 0.1, feature_count)
+        
+    def hinge_loss(self, X, y):
+        '''
+        Compute the hinge-loss for input data X (shape (n, m)) with target y (shape (n,)).
+
+        Returns a length-n vector containing the hinge-loss per data point.
+        '''
+        # Implement hinge loss
+        return None
+
+    def grad(self, X, y):
+        '''
+        Compute the gradient of the SVM objective for input data X (shape (n, m))
+        with target y (shape (n,))
+
+        Returns the gradient with respect to the SVM parameters (shape (m,)).
+        '''
+        # Compute (sub-)gradient of SVM objective
+        return None
+
+    def classify(self, X):
+        '''
+        Classify new input data matrix (shape (n,m)).
+
+        Returns the predicted class labels (shape (n,))
+        '''
+        # Classify points as +1 or -1
+        return None
+
+def load_data():
+    '''
+    Load MNIST data (4 and 9 only) and split into train and test
+    '''
+    mnist = fetch_mldata('MNIST original', data_home='./data')
+    label_4 = (mnist.target == 4)
+    label_9 = (mnist.target == 9)
+
+    data_4, targets_4 = mnist.data[label_4], np.ones(np.sum(label_4))
+    data_9, targets_9 = mnist.data[label_9], -np.ones(np.sum(label_9))
+
+    data = np.concatenate([data_4, data_9], 0)
+    data = data / 255.0
+    targets = np.concatenate([targets_4, targets_9], 0)
+
+    permuted = np.random.permutation(data.shape[0])
+    train_size = int(np.floor(data.shape[0] * 0.8))
+
+    train_data, train_targets = data[permuted[:train_size]], targets[permuted[:train_size]]
+    test_data, test_targets = data[permuted[train_size:]], targets[permuted[train_size:]]
+    print("Data Loaded")
+    print("Train size: {}".format(train_size))
+    print("Test size: {}".format(data.shape[0] - train_size))
+    print("-------------------------------")
+    return train_data, train_targets, test_data, test_targets
+
+def optimize_test_function(optimizer, w_init=10.0, steps=200):
+    '''
+    Optimize the simple quadratic test function and return the parameter history.
+    '''
+    def func(x):
+        return 0.01 * x * x
+
+    def func_grad(x):
+        return 0.02 * x
+
+    w = w_init
+    w_history = [w_init]
+
+    for _ in range(steps):
+        # Optimize and update the history
+        pass
+    return w_history
+
+def optimize_svm(train_data, train_targets, penalty, optimizer, batchsize, iters):
+    '''
+    Optimize the SVM with the given hyperparameters. Return the trained SVM.
+    '''
+    return None
+
+if __name__ == '__main__':
+    pass
